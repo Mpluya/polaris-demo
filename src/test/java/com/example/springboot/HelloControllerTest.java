b57c2f36013c02e3f39746d174d7b28f4f2ee811
Mae Large <mlarge@vmware.com>
2022-08-20 09:32:32 -0700

    removed security vuln

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -26,6 +26,6 @@ class HelloControllerTest {
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Should work as we want it to!"git a));
+            .andExpect(content().string("Should work as we want it to!"));
     }
 }
