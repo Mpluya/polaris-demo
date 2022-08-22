9fa89ad23132aa91df61434afd3da62f87c26fce
Mae Large <mlarge@vmware.com>
2022-08-22 11:03:28 -0700

    another security vuln

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
+	private static final String MY_SECRET = "47828a8dd77ee1eb9dde2d5e93cb221ce8c32b37";
+	
 	@RequestMapping("/")
 	public String index() {
-
+		System.out.println(MY_SECRET);
 		return "I love sonar!";
 	}
 
